01d3a8f493a4315e094aeb7c40f1a6a5dc6a85d5
Your Name <you@example.com>
2018-01-12 12:37:02 +0100

    Add files to project

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+
+var Increment = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 0
+    };
+  },
+
+  increment: function() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+  
+  decrement: function(){
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
+
+  render: function() {
+    return <div>
+      <div id='counter'>{this.state.counter}</div> 
+      <button onClick = {this.increment}> Dodaj 1 </button> 
+      <button onClick = {this.decrement}> Minus 1 </button> 
+     </div>
+  }
+});
+
+React.render(<Increment />, document.getElementById('app'));
